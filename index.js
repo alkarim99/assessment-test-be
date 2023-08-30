e723ff3dd5df706772f066854eb2cfd725817fa0
Abdullah Al-Karim Amrullah <abdullahalkarimamrullah01@gmail.com>
2023-08-30 09:51:05 +0700

    Add cors

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require("dotenv").config()
 const express = require("express")
 const app = express()
 const bodyParser = require("body-parser")
+const cors = require("cors")
 
 const candidateRoutes = require("./routes/candidate.routes")
 
@@ -13,6 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 // parse application/json
 app.use(bodyParser.json())
+app.use(cors())
 
 app.use(candidateRoutes)
 
